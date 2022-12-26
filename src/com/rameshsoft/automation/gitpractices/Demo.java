cb62b739518efa42fcf7303afb3c497300972a26
rameshsoft9177791456 <rameshsoft.9177791456@gmail.com>
2022-12-26 20:30:53 +0530

    added new files by automation dev

--- src/com/rameshsoft/automation/gitpractices/Demo.java
+++ src/com/rameshsoft/automation/gitpractices/Demo.java
@@ -4,5 +4,6 @@ public class Demo {
 public void hello() {
 	System.out.println("Hello");
 	System.out.println("JAVA");
+System.out.println("JAVA456");
 }
 }
---
+++ src/com/rameshsoft/automation/gitpractices/GitDemoTest.java
@@ -0,0 +1,9 @@
+package com.rameshsoft.automation.gitpractices;
+
+public class GitDemo {
+public static void main(String[] args) {
+	
+	System.out.println("Hello do hardwork");
+	
+}
+}
