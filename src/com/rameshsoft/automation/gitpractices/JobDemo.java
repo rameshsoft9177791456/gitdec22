7483e67a5b577feb2e0e206e3d6e5eb698263ee4
rameshsoft9177791456 <rameshsoft.9177791456@gmail.com>
2022-12-28 09:04:25 +0530

    job

--- src/com/rameshsoft/automation/gitpractices/JobDemo.java
+++ src/com/rameshsoft/automation/gitpractices/JobDemo.java
@@ -4,6 +4,8 @@ public class JobDemo {
 public static void main(String[] args) {
 	
 	System.out.println("JOB");
-	
+	System.out.println("Hardwork");
+	System.out.println("JOB");
+	System.out.println("PRACTICE");
 }
 }
