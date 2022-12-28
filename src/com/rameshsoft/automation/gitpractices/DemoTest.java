88d5ee81c15a57757d05ecc038be69672eed3893
rameshsoft9177791456 <rameshsoft.9177791456@gmail.com>
2022-12-28 08:55:21 +0530

    hello

--- src/com/rameshsoft/automation/gitpractices/DemoTest.java
+++ src/com/rameshsoft/automation/gitpractices/DemoTest.java
@@ -3,5 +3,6 @@ package com.rameshsoft.automation.gitpractices;
 public class DemoTest {
 public void job() {
 	System.out.println("job");
+	System.out.println("Job");
 }
 }
---
+++ src/com/rameshsoft/automation/gitpractices/Job.java
@@ -0,0 +1,7 @@
+package com.rameshsoft.automation.gitpractices;
+
+public class Job {
+public void hello() {
+	System.out.println("Hello do hardwork");
+}
+}
